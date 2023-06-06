0cf76d3951cea8171bcbf8189a1a48c69be7fc9e
Timothy Wilger <47797775+tofu-tim@users.noreply.github.com>
2023-06-05 21:54:50 -0500

    Add files via upload
    added more functionality and made code easier to read

--- AlgorithmPracticeGetPrimes.js
+++ AlgorithmPracticeGetPrimes.js
@@ -1,12 +1,31 @@
-// Given a single integer 'n', find all prime numbers from 0 to 'n'
+// Given a single integer 'n', find all prime numbers from 1 to 'n'
 function getPrimes(integer) {
     let allNumberArray = [];
+    let listOfPrimes = [];
 
-    for (var i = 1; i <= integer; i++) {
+    for (var i = 2; i <= integer; i++) {
         allNumberArray.push(i);
     }
-    return allNumberArray;
+
+    function checkIfDivisible(number) {
+        for (var x = 2; x < allNumberArray.length; x++) {
+            if (number % allNumberArray[x] === 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    for (var i = 2; i <= integer; i++) {
+        if (checkIfDivisible(i)) {
+            listOfPrimes.push(i);
+        }
+    }
+
+    return listOfPrimes;
 }
-let test1 = 20
+
+
+let test1 = 100
 console.log('Integer = ' + test1);
-console.log(getPrimes(test1));
+console.log('The prime number from 0 to ' + test1 + ' are: ' + getPrimes(test1));
